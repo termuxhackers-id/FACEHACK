6c16d8b294d4e97608b6c3e102d53ed69e68f5ef
Termux Hackers <82352535+termuxhackers-id@users.noreply.github.com>
2024-02-10 23:53:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 pip install bs4 rich requests httpagentparser licensing
-rm data/dump/*.md data/rest/*.md data/auth/*.md data/ugen/*.md
+find . -name '*.md' -delete
+python3 fhack.py
